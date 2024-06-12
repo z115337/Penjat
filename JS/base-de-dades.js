32d2aefa601191daaf65f14fa21f53d5f0e351db
z115337 <157492898+z115337@users.noreply.github.com>
2024-06-12 17:53:46 +0200

    base-de-dades.js

--- JS/base-de-dades.js
+++ JS/base-de-dades.js
@@ -141,10 +141,10 @@ var pista = pistes[paraulespistes[aleatori]];
 
 aleatori = Math.floor(Math.random() * Taula.length);
 paraula = Taula[aleatori].Paraula;
-pista = Taula[0].Pista;
-//pista = Taula[aleatori].Pista;
+pista = Taula[aleatori].Pista;
+//pista = Taula[0].Pista;
 //window.alert("aleatori=" + aleatori + ", paraula=" + paraula + ", pista=" + pista);
-window.alert(Let's go: a la quinta forca / al quinto pino / to the boondocks?);
+window.alert(Let us go: a la quinta forca / al quinto pino / to the boondocks?);
 
 for (var i = 0; i < paraula.length; i++) { 
     encertats[i] = "_";
